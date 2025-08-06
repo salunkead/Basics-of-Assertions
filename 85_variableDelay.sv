6a8dbb775f8619c88b12df2a70786ccd38081230
Amol Salunke <salunkeamol10@gmail.com>
2025-08-06 17:31:00 +0530

    Create 85_variableDelay.sv

---
+++ 85_variableDelay.sv
@@ -0,0 +1,18 @@
+//variable clock delay
+
+/*
+##delay 
+delay cannot be variable as the delay value must be known before run time.
+
+##[0:lv_data]
+in the range operator also variable delay is not possible.
+
+in short,delay values must be constants and it cannot be variables.
+
+what if you want variable delay value?
+1. this can be done using local_variable
+
+*/
+
+//Example:-
+
